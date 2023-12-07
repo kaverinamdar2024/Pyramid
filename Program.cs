f34d9d84dc37a0171446b9b0e94804247dd3f238
kaverinamdar2024 <kaverinamdar@gmail.com>
2023-12-07 20:35:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!")
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of rows for the pyramid");
+        int numrows=int.Parse(Console.ReadLine());
+        for(int i=1; i<=numrows;i++)
+        {
+            for (int j = 1; j <= numrows - i; j++)
+            {
+                Console.Write( "");
+            }
+            for (int k=1;k<=i;k++)
+            {
+                Console.Write(  "*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
